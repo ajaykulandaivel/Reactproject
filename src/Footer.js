3e056e2e8a93475dd9b43e7c782814ee46f876c4
ajaykulandaivel <108710474+ajaykulandaivel@users.noreply.github.com>
2023-04-21 15:27:58 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='foot'>
+        <h4>CopyRights @ ALL RIGHTS RESERVED</h4>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='head'>
+        <h1>Random Quote Generator</h1>
+        <h5></h5>
+    </div>
+  )
+}
+
+export default Header
